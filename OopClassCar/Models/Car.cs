660e8414b49d88f6350f68f6c07f16570089a8ba
Ieva Staseviciute <ievastaseviciu@gmail.com>
2025-03-06 12:49:42 +0200

    car object

---
+++ OopClassCar/Models/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopClassCar.Models
+{
+    class Car
+    {
+        public string? Brand { get; set; }
+        public int? Year { get; set; }
+
+        public Car(string brand, int year)
+        {
+            Brand = brand;
+            Year = year;
+        }
+    }
+}
---
+++ OopClassCar/Program.cs
@@ -0,0 +1,23 @@
+﻿//Užduotis:
+//Sukurkite klasę Car, kuri turės savybes Brand ir Year.
+//Sukurkite objektą ir priskirkite jam reikšmes.
+//Išveskite informaciją apie automobilį į konsolę.
+
+
+using OopClassCar.Models;
+
+namespace OopClassCar
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Car car = new Car("bmw", 2026);
+
+            Console.WriteLine(car.Brand);
+            Console.WriteLine(car.Year);
+        }
+
+  
+    }
+}
