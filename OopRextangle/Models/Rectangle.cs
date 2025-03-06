3ceb8affefd191c74e9f8f8ee25f0aa97bd906c3
Ieva Staseviciute <ievastaseviciu@gmail.com>
2025-03-06 16:29:10 +0200

    rectangle area with constructor and method

---
+++ OopRextangle/Models/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopRectangle.Models
+{
+    public class Rectangle
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public Rectangle(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double GetArea()
+        {
+            return Height * Width;
+        }
+    }
+}
---
+++ OopRextangle/Program.cs
@@ -0,0 +1,19 @@
+﻿//Užduotis:
+//Sukurkite klasę Rectangle, kuri turės savybes Width ir Height.
+//Sukurkite metodą GetArea(), kuris grąžina stačiakampio plotą.
+//Sukurkite objektą ir išveskite jo plotą į konsolę.
+
+using OopRectangle.Models;
+
+namespace OopRectangle
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Rectangle rectangle = new Rectangle(10, 20);
+            Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+
+        }
+    }
+}
