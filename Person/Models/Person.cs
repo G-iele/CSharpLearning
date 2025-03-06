9c470a2ef9bc4ecc65ab4ef57a3b45a302c92f7d
Ieva Staseviciute <ievastaseviciu@gmail.com>
2025-03-06 16:22:01 +0200

    person class with constructor

---
+++ Person/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopPerson.Models
+{
+    public class Person
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void PrintPerson()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Age: {Age}");
+        }
+    }
+}
---
+++ Person/Program.cs
@@ -0,0 +1,20 @@
+﻿//Užduotis:
+//Sukurkite klasę Person, kuri turės savybes Name ir Age.
+//Sukurkite konstruktorių, kuris priima šias reikšmes kaip parametrus.
+//Sukurkite objektą ir išveskite informaciją į ekraną.
+
+
+
+using OopPerson.Models;
+
+namespace OopPerson
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Person person = new Person("Laura", 20);
+            person.PrintPerson();
+        }
+    }
+}
