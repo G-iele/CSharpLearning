1925e76fce56251f5b28d4443cf9f45011c99b57
Ieva Staseviciute <ievastaseviciu@gmail.com>
2025-03-06 16:52:01 +0200

    washing machine class, constructor, method

---
+++ OopWashingMachine/Models/WashingMachine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopWashingMachine.Models
+{
+    public class WashingMachine
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public int EnergyConsumptionPerCycle { get; set; }
+
+        public WashingMachine (string brand, string model, int energyConsumptionPerCycle)
+        {
+            Brand = brand;
+            Model = model;
+            EnergyConsumptionPerCycle = energyConsumptionPerCycle;
+        }
+
+        public double CalculateTotalEnergyUsage(int cycles)
+        {
+            return EnergyConsumptionPerCycle * cycles;
+        }
+    }
+}
---
+++ OopWashingMachine/Program.cs
@@ -0,0 +1,24 @@
+﻿//Skalbimo Mašinos Energijos Sąnaudų Skaičiavimas
+//Užduotis:
+//Sukurkite klasę WashingMachine, kuri turės savybes:
+//Brand(gamintojas)
+//Model(modelis)
+//EnergyConsumptionPerCycle(energijos suvartojimas per vieną skalbimo ciklą, kWh)
+//Sukurkite konstruktorių, kuris inicializuos visas šias reikšmes.
+//Sukurkite metodą CalculateTotalEnergyUsage(int cycles), kuris apskaičiuos ir grąžins bendrą
+//sunaudotą energiją per nurodytą ciklų skaičių.
+//Programoje sukurkite WashingMachine objektą, apskaičiuokite sunaudotą energiją per 15 skalbimo ciklų ir išveskite rezultatą.
+
+using OopWashingMachine.Models;
+
+namespace OopWashingMachine
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            WashingMachine washingMachine = new WashingMachine("Philips", "R1", 20);
+            Console.WriteLine($"{washingMachine.Brand} | {washingMachine.Model} | {washingMachine.CalculateTotalEnergyUsage(15)}");
+        }
+    }
+}
