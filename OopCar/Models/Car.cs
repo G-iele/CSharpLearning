c01be5e81e0b7e1a1a3c9bd6415f58bbf80e11a7
Ieva Staseviciute <ievastaseviciu@gmail.com>
2025-03-06 16:41:37 +0200

    car class, constructor, method

---
+++ OopCar/Models/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopCar.Models
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public int FuelConsumptionPer100km { get; set; }
+
+        public Car(string brand, string model, int fuelConsumptionPer100km)
+        {
+            Brand = brand;
+            Model = model;
+            FuelConsumptionPer100km = fuelConsumptionPer100km;
+        }
+
+        public double CalculateFuelUsage(double distance)
+        {
+            return (FuelConsumptionPer100km * distance) / 100; 
+        }
+
+    }
+}
---
+++ OopCar/Program.cs
@@ -0,0 +1,24 @@
+﻿//Automobilio Klasė su Kuro Sąnaudų Skaičiavimu
+//Užduotis:
+//Sukurkite klasę Car, kuri turės šias savybes:
+//Brand(gamintojas)
+//Model(modelis)
+//FuelConsumptionPer100km(kuro sąnaudos 100 km)
+//Sukurkite konstruktorių, kuris inicializuos visas šias reikšmes.
+//Sukurkite metodą CalculateFuelUsage(double distance), kuris apskaičiuos ir grąžins reikalingą
+//kuro kiekį tam tikram atstumui nuvažiuoti.
+//Programoje sukurkite automobilio objektą, apskaičiuokite kuro sunaudojimą 350 km ir išveskite rezultatą.
+
+using OopCar.Models;
+
+namespace OopCar
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Car car = new Car("Volvo", "Nice model", 12);
+            Console.WriteLine($"Brand: {car.Brand} | Model: {car.Model} | Takes fuel for 50 km: {car.CalculateFuelUsage(50)}");
+        }
+    }
+}
